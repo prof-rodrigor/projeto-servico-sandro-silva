e7435f606455b67743004bf3429c9ce64a1fc7d6
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2024-09-20 00:31:18 -0300

    Implemantando classe ValidadorInteiro

---
+++ src/main/java/br/ufpb/dcx/rodrigor/servico/participantes/model/ValidadorInteiro.java
@@ -0,0 +1,12 @@
+package br.ufpb.dcx.rodrigor.servico.participantes.model;
+
+public class ValidadorInteiro {
+    private int min;
+    private int max;
+
+
+    public ValidadorInteiro(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+}
