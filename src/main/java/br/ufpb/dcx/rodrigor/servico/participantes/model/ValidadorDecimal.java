a64f1ed6d38a5a4df5856b966d4feef2acf2c652
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2024-09-21 00:15:09 -0300

    Implemantando classe ValidadorDecimal

---
+++ src/main/java/br/ufpb/dcx/rodrigor/servico/participantes/model/ValidadorDecimal.java
@@ -0,0 +1,17 @@
+package br.ufpb.dcx.rodrigor.servico.participantes.model;
+
+public class ValidadorDecimal implements ValidadorCampo {
+    private final double min;
+    private final double max;
+
+
+    public ValidadorDecimal(double min, double max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public boolean validar(String valor) {
+        return false;
+    }
+}
