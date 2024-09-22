b084beaaac33b31d5fddd0d13aeb9f52c43ab949
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2024-09-21 23:40:59 -0300

    Implemantando classe FormularioService

--- src/main/java/br/ufpb/dcx/rodrigor/servico/participantes/services/FormularioService.java
+++ src/main/java/br/ufpb/dcx/rodrigor/servico/participantes/services/FormularioService.java
@@ -5,6 +5,7 @@ import br.ufpb.dcx.rodrigor.servico.participantes.model.Form;
 import br.ufpb.dcx.rodrigor.servico.participantes.model.ValidadorDecimal;
 
 public class FormularioService {
+
     // Método que retorna um formulário com base no ID
     public Form getFormulario(String id) {
         // Cria um novo formulário com título e id fornecido
@@ -12,7 +13,7 @@ public class FormularioService {
 
         // Adiciona campos ao formulário
         //formulario.addCampo(new Campo("nome", "Nome", new ValidadorTexto(1, 100), true));
-        //formulario.addCampo(new Campo("email", "E-mail", new ValidadorEmail(), true));
+        formulario.addCampo(new Campo("email", "E-mail", new ValidadorEmail(), true));
         formulario.addCampo(new Campo("altura", "Altura", new ValidadorDecimal(0.5, 2.5), true));
         //formulario.addCampo(new Campo("data_nascimento", "Data de Nascimento", new ValidadorData(), true));
 
